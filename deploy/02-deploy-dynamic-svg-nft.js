42419ad49c131479bfe6f2fc7d66fec74087db87
HOSANNA <hosanna@192.168.2.75>
2023-06-01 20:11:29 +0800

    initial commit

---
+++ deploy/02-deploy-dynamic-svg-nft.js
@@ -0,0 +1,40 @@
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+const fs = require("fs")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const EthUsdAggregator = await ethers.getContract("MockV3Aggregator")
+        ethUsdPriceFeedAddress = EthUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
+    }
+
+    const lowSVG = fs.readFileSync("./images/dynamicNft/frown.svg", { encoding: "utf8" })
+    const highSVG = fs.readFileSync("./images/dynamicNft/happy.svg", { encoding: "utf8" })
+
+    log("----------------------------------------------------")
+
+    args = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+    const dynamicSvgNft = await deploy("DynamicSvgNft", {
+        from: deployer,
+        log: true,
+        args: args,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(dynamicSvgNft.address, args)
+    }
+}
+
+module.exports.tags = ["all", "dynamicsvg", "main"]
